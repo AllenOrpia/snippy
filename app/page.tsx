326a65531759d57cfec2d9f0b2b8d8f498179fe9
Allen Orpia <allenorpia17@gmail.com>
2025-01-07 15:51:41 -0800

    minor fix for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,12 @@
-import {
-  SignedOut,
-  SignUpButton,
-  SignedIn,
-  SignOutButton,
-  UserButton
-} from "@clerk/nextjs";
-import { User } from "@clerk/nextjs/server";
-import Image from "next/image";
+
+
 
 export default function Home() {
   console.log("Hey from");
   return (
     <div className="">
       Home Page
-      <SignedOut>
-        <SignUpButton />
-      </SignedOut>
+      
     </div>
   );
 }
